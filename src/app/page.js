071b83a21e00d766a44e64abd5f4221f6fab128a
thy <mailcuathy@gmail.com>
2024-08-10 03:25:30 +0700

    chore: update layout and home pages with MUI components; add custom theme

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+import Box from "@mui/material/Box";
+
 export default function Home() {
-  return <main></main>;
+  return <Box></Box>;
 }
---
+++ src/theme.js
@@ -0,0 +1,24 @@
+"use client";
+import { Roboto } from "next/font/google";
+import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const theme = extendTheme({
+  colorSchemes: {
+    light: {},
+    dark: {},
+  },
+
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+
+  components: {},
+});
+
+export default theme;
